96ff6284ef3fb186db8eb9455dada45a0716ea6c
Ignacio Merani <imerani@gmail.com>
2018-09-30 15:21:55 -0400

    examples done!

--- limits-service/src/main/java/com/tracfone/microservices/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/tracfone/microservices/LimitsConfigurationController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 /**
  * @author imerani
  *
@@ -21,4 +23,16 @@ public class LimitsConfigurationController {
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Service not available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(100,1);
+	}
+	
+	
 }
